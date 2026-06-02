41cf4054336a386ffe2ae4f50d3624b539022b22
Tirth S Pandya <teamfznltd@gmail.com>
2026-06-02 13:14:15 +0530

    ....

---
+++ FE/src/pages/Register.tsx
@@ -0,0 +1,14 @@
+
+
+const Register = () => {
+  return (
+    <div style={{display: 'flex', flexDirection: 'row'}}>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Register
